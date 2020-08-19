848ded73db57c1e52edd0c69afd767f9dfa6cb6b
abdellihou <abdelli.hou@gmail.com>
2020-08-19 18:46:15 +0100

    raté

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,18 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <Landing />
   </div>
 </template>
 
 <script>
-
+import Landing from "./components/Landing";
 
 export default {
   name: "App",
   components: {
+    Landing,
   },
 };
 </script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,35 +3,38 @@ import Vue from "vue";
 import Vuesax from "vuesax";
 import "vuesax/dist/vuesax.css";
 import "material-icons/iconfont/material-icons.css";
-import VideoBackground from "vue-responsive-video-background-player";
-import { Plugin } from "vue-responsive-video-background-player";
 import MenuIcon from "vue-material-design-icons/Menu.vue";
 import VueMaterial from "vue-material";
 import "vue-material/dist/vue-material.min.css";
 import "vue-material/dist/theme/default.css";
 import VueRouter from "vue-router";
-import Profile from "./components/Profile.vue";
-import Home from "./components/Home.vue";
-import LoginSM from "./components/LoginSM.vue";
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.component("video-background", VideoBackground, MenuIcon);
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+// import Profile from "./components/Profile.vue";
+// import Home from "./components/Home.vue";
+// import LoginSM from "./components/LoginSM.vue";
+Vue.component( MenuIcon);
 Vue.use(VueMaterial);
-Vue.use(Plugin);
 Vue.use(Vuesax, {});
 Vue.use(VueRouter);
 
-const routes = [
-  { path: "/profile", component: Profile },
-  { path: "/", component: Home },
-  { path: "/login", component: LoginSM },
-];
+// const routes = [
+//   { path: "/profile", component: Profile },
+//   { path: "/", component: Home },
+//   { path: "/login", component: LoginSM },
+// ];
 
-const router = new VueRouter({
-  routes,
-  mode: "history",
-});
+// const router = new VueRouter({
+//   routes,
+//   mode: "history",
+// });
 
 new Vue({
-  router,
+  // router,
   render: (h) => h(app),
 }).$mount("#app");
