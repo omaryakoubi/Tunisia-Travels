b82df11ec06f7e827afdf2c6602eb234897b7b48
firasmr997 <firasmr997@gmail.com>
2020-08-20 10:29:27 +0100

    Ferr

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,6 @@ import Vue from "vue";
 import Vuesax from "vuesax";
 import "vuesax/dist/vuesax.css";
 import "material-icons/iconfont/material-icons.css";
-import VideoBackground from "vue-responsive-video-background-player";
-import { Plugin } from "vue-responsive-video-background-player";
 import MenuIcon from "vue-material-design-icons/Menu.vue";
 import VueMaterial from "vue-material";
 import "vue-material/dist/vue-material.min.css";
@@ -14,16 +12,15 @@ import Profile from "./components/Profile.vue";
 import Home from "./components/Home.vue";
 import LoginSM from "./components/LoginSM.vue";
 
-Vue.component("video-background", VideoBackground, MenuIcon);
+Vue.component( MenuIcon);
 Vue.use(VueMaterial);
-Vue.use(Plugin);
 Vue.use(Vuesax, {});
 Vue.use(VueRouter);
 
 const routes = [
   { path: "/profile", component: Profile },
   { path: "/", component: Home },
-  { path: "/login", component: LoginSM },
+  { path: "/login", component: LoginSM }
 ];
 
 const router = new VueRouter({
